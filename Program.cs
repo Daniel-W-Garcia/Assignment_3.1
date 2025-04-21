f95067836cc92fc0df49b687c7227734abdb9257
itodobien <daniel.w.garcia@outlook.com>
2025-04-21 09:20:03 -0700

    Complete

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Text;
+
+string ReturnEvenNumbers()
+{
+    StringBuilder sb = new StringBuilder();
+    
+    for (int i = 2; i < 100; i += 2)
+    {
+        sb.Append(i);
+        if (i < 98)
+        {
+            sb.Append(", ");
+        }
+
+    }
+    return sb.ToString();
+}
+
+Console.WriteLine(ReturnEvenNumbers());
